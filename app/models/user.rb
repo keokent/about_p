b18af482fbdf1b1991fc04add204a23e8160cea0
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 04:48:00 +0900

    ActiveRecordにケースを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
+  extend Enumerize
+
   belongs_to :section
   validates :name, presence: true
   validates :section_id, presence: true
   validates :job_type, presence: true
   validates :github_id, presence: true
   validates :irc_name, presence: true
+
+  enumerize :job_type, :in => [:engineer, :designer, :backoffice, :manager, :producer]
 end
