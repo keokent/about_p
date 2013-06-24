aa7a1aeb49573c075ae6737eba533a3d4dd1246f
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 05:47:11 +0900

    認証のテストのとっかかりをつくった

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in with Github') }
+  end
+end
