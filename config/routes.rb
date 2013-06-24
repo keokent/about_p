4d07656f487eca7aac03646348a7ca1983ce7068
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-24 22:50:25 +0900

    add signout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ AboutP::Application.routes.draw do
 
   match "/auth/github/callback", to: "sessions#callback", via: 'get'
   match "/signin", to: "sessions#new", via: 'get'
+  match "/signout", to: "sessions#destroy", via: 'delete'
 end
