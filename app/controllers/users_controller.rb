5d6c4765e1863333214370738a66be341db687aa
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-24 21:50:00 +0900

    ユーザ作成ページのひな形を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  # TODO: session[:github_id]がnilだったら/signinへリダイレクトするbefore_acitonを実装
   def new
+    @user = User.new(github_id: session[:github_id])
   end
 end
