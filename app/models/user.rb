400b0bbe9960802a15d64045c4d61f4d52fcadea
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 06:02:36 +0900

    認証で必要なヘルパーを追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   validates :job_type, presence: true
   validates :github_id, presence: true
   validates :irc_name, presence: true
+  before_save :create_random_token
+
+  private
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
