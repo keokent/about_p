117b90c0fc3264d2f7490c53896191dc384b40f4
keoken <keokenppb@gmail.com>
2013-06-24 17:12:26 +0900

    ユーザモデルの所属、職種等の属性のvalidationテストの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   belongs_to :section
   validates :name, presence: true
+  validates :section_id, presence: true
+  validates :job_type, presence: true
+  validates :github_id, presence: true
+  validates :irc_name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,26 @@ describe User do
     before { @user.name = "" }
     it { should_not be_valid }
   end
+
+  describe "when section_id is not present" do
+    before { @user.section_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when job_type is not present" do
+    before { @user.job_type = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when github_id is not present" do
+    before { @user.github_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when irc_name is not present" do
+    before { @user.irc_name = "" }
+    it { should_not be_valid }
+  end
 end
 
   
