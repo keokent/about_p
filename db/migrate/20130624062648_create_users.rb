7bf4e381c38919c8590c590338d276380f2feafc
keoken <keokenppb@gmail.com>
2013-06-24 15:35:35 +0900

    ユーザテーブルのマイグレーションスクリプトを書いた

---
+++ db/migrate/20130624062648_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :nickname
+      t.string :irc_name
+      t.integer :section_id
+      t.integer :job_type
+      t.string :github_id
+      t.string :birthday
+      t.text :backgroud
+      t.text :hobby
+      t.text :free_space
+      t.timestamps
+    end
+  end
+end
