e1169764e8c8babe3ad54653399ea166d82735b4
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 05:50:40 +0900

    Githubで認証していないのにユーザ作成ページへ移動したらsigninページへリダイレクトすることを確認するテストを追記

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,4 +9,10 @@ describe "Authentication" do
 
     it { should have_content('Sign in with Github') }
   end
+
+  context "not throught github authentication" do
+    before { visit new_user_path }
+
+    it { should have_content('Sign in with Github') }
+  end
 end
