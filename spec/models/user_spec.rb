ec8786b2cf9884b37da6b210e0ded8f5eec86896
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-24 16:55:02 +0900

    nameが空文字のときにinvalidかどうか確かめるテストを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Kenta Takeo",
+  before do
+    @user = User.new(name: "Kenta Takeo",
                             section_id: 1,
                             job_type: 1,
                             github_id: "1",
-                            irc_name: "keoken") }
+                            irc_name: "keoken")
+  end
+
   subject { @user }
 
   it { should respond_to(:name) }
@@ -13,6 +16,11 @@ describe User do
   it { should respond_to(:job_type) }
   it { should respond_to(:github_id) }
   it { should respond_to(:irc_name) }
+
+  describe "when name is not present" do
+    before { @user.name = "" }
+    it { should_not be_valid }
+  end
 end
 
   
