a2843a7c6973e430762297ca94609321693d4178
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 05:27:44 +0900

    githubで認証せずに新規ユーザ作成ページに移動したらログインページへ移動

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
-  # TODO: session[:github_id]がnilだったら/signinへリダイレクトするbefore_acitonを実装
+  before_action :through_github, only: [:new]
+
   def new
     @user = User.new(github_id: session[:github_id])
+  end
+
+  # Before actions
+  def through_github 
+    redirect_to(signin_path) unless session[:github_id]
   end
 end
