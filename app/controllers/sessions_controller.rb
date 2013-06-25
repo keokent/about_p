43330f84e356e43836cd249f474ea1388021c53e
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-25 10:53:33 +0900

    unlessの代わりにifを使って節を入れ替える（可読性）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     # TODO: https://github.com/paperboy-all/all/blob/master/github/members.txtから社員のGithubデータを取得して, auth["info"]["nickname"]に該当しなかったら弾く
     user = User.find_by(github_id: auth["uid"])
-    unless user
-      session[:github_id] = auth["uid"]
-      redirect_to new_user_path
-    else
+    if user
       sign_in user
       redirect_to user_path(user)
+    else
+      session[:github_id] = auth["uid"]
+      redirect_to new_user_path
     end
   end
 
