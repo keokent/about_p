2779e253b9ae33444b87f44a36a940f3d67d6a98
keoken <keokenppb@gmail.com>
2013-06-25 17:36:50 +0900

    newページの自己紹介内容の追加とテストの追加

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+	
+describe "UserPages" do
+  
+  before do
+    %w(ホスティング事業本部 ロリポップ！ ムームードメイン インフラチーム
+       福岡カスタマーサービス minne 福岡支社付 EC事業本部 カラーミーショップ
+       カラメル 運営支援 グーペ ECカスタマーサービス メディア事業本部 JUGEM heteml 
+       スマホサービス 事業開発 petit 経営管理本部 法務 総務 経理財務
+       情報システム部 人材開発本部 国際化推進室 ジュゲムカート 内部監査室
+       社長室 技術基盤 ブクログ).each do |name|
+      # TODO: iconは用意できたら読み込む
+      Section.create(name: name)
+    end
+  end
+
+  subject { page  }
+  
+  describe "index" do
+
+  end
+
+  describe "new" do 
+    before { sign_in }
+    describe "no user" do
+      it "ユーザ登録ベージにいるべき" do
+        expect(page).to have_button('Create User')
+      end
+    end
+
+    describe "不正なデータが入力されたとき" do
+      before do
+        fill_in "Name", with: " "
+        select "JUGEM", :from => "Section"
+        select "Engineer", :from => "Job type"
+        fill_in "Irc name", with: "12345"
+        click_button 'Create User'
+      end
+
+      it "エラーが表示されるべき" do
+        expect(page).to have_button('Create User')
+        expect(page).to have_content('contains 1 error')
+      end
+    end	   
+  end
+end
