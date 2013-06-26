4ec5b6b884ace61679c0a1b33c1c3af58ee896b7
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-26 16:42:56 +0900

    implement authorization

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,6 +18,10 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: cookies[:remember_token])
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
