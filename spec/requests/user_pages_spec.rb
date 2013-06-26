b2767b0d461f9b153eba50b0e6b40ab59723dce0
Keisuke KITA <kei.kita2501@gmail.com>
2013-06-26 11:55:45 +0900

    編集ページにフォーム部品があることを確認するテストを追記

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,12 +10,11 @@ describe "UserPages" do
        スマホサービス 事業開発 petit 経営管理本部 法務 総務 経理財務
        情報システム部 人材開発本部 国際化推進室 ジュゲムカート 内部監査室
        社長室 技術基盤 ブクログ).each do |name|
-      # TODO: iconは用意できたら読み込む
       Section.create(name: name)
     end
   end
 
-  subject { page  }
+  subject { page }
   
   describe "index" do
 
@@ -23,6 +22,7 @@ describe "UserPages" do
 
   describe "new" do 
     before { sign_in }
+
     describe "no user" do
       it "ユーザ登録ベージにいるべき" do
         expect(page).to have_button('登録する')
@@ -31,7 +31,6 @@ describe "UserPages" do
 
     describe "不正なデータが入力されたとき" do
       before do
-        puts page.html
         fill_in "名前", with: " "
         select "JUGEM", :from => "部署"
         select "エンジニア", :from => "職種"
@@ -40,10 +39,25 @@ describe "UserPages" do
       end
 
       it "エラーが表示されるべき" do
-        puts page.html
         expect(page).to have_button('登録する')
         expect(page).to have_content('error')
       end
     end	   
+  end
+
+  describe "edit" do
+    before do
+      @section = Section.find_by(name: "人材開発本部")
+      @user = @section.users.create(name: "喜多啓介",
+                                    job_type: :engineer,
+                                    github_uid: "12345",
+                                    irc_name: "kitak",
+                                    hometown: "渋谷")
+      sign_in @user
+      visit edit_user_path(@user)
+    end 
+
+    it { should have_button('更新する') }
+    it { should have_selector("input#user_name[value='喜多啓介']")}
   end
 end
