057d8dc9121e448c5f37abd6ef7297b4b1698ae3
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-16 20:37:17 +0900

    新規登録とプロフィール更新時にIRCで更新を送る

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  include AboutP::Utilities
   extend Enumerize
 
   belongs_to :section
@@ -10,10 +11,20 @@ class User < ActiveRecord::Base
   validates :twitter_id, length: { maximum: 15 }, format: { with: /\A[0-9a-zA-Z\_]*\Z/ }
   
   before_save :create_remember_token
+  after_create :notify_newcomer, :if => ->{ Rails.env.production? }
+  after_update :notify_update, :if => ->{ Rails.env.production? }
 
   private
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
+  end
+
+  def notify_newcomer
+    ikachan("#{self.nickname} さんがabout pをはじめました: http://about-p-kitak.sqale.jp/users/#{self.id}")
+  end
+
+  def notify_update
+    ikachan("#{self.nickname} さんがabout pのプロフィールを更新しました: http://about-p-kitak.sqale.jp/users/#{self.id}")
   end
 
   enumerize :job_type, :in => [:engineer, :designer, :director, :customer_service, :backoffice, :producer, :manager, :officer, :other]
