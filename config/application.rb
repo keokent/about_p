5f9ac3079fc3b73a34abe1cc4105028ab449f8e9
Keisuke KITA <kei.kita2501@gmail.com>
2013-07-17 09:12:38 +0900

    fix path

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module AboutP
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.autoload_paths += %W(#{Rails.root}/lib/about_p)
+    config.autoload_paths += %W(#{Rails.root}/lib)
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
