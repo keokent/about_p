e0ff90e528fd164ce619f5c3df352e1762df7fe7
keoken <keokenppb@gmail.com>
2013-08-22 20:54:13 +0900

    部署名変更のマイグレーションスクリプトを作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130625061842) do
+ActiveRecord::Schema.define(version: 20130822102456) do
 
   create_table "sections", force: true do |t|
     t.string   "name"
